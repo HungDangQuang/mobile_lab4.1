919046bcd6f663427aa9d686c979d6aa7c00b844
HungDangQuang <hungdangkg@gmail.com>
2022-05-10 09:06:28 +0700

    first commitment

---
+++ app/src/main/java/uit/app/a18520790_dangquanghung_lab41/MainActivity.java
@@ -0,0 +1,14 @@
+package uit.app.a18520790_dangquanghung_lab41;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
